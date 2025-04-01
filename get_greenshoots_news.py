b23af9be3ab4519f4f41e8ed0be319aeffefe058
rrvfqo <rrvfqo@gmail.com>
2025-04-01 15:01:49 +0800

    更新 GitHub Actions 工作流程為每4小時執行，調整 Google 新聞檢查的查詢時間

--- get_greenshoots_news.py
+++ get_greenshoots_news.py
@@ -6,35 +6,10 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-import json
-import os
 
-# 紀錄已發送的新聞
-sent_news_file = 'sent_news.json'
-if os.path.exists(sent_news_file):
-    with open(sent_news_file, 'r', encoding='utf-8') as f:
-        sent_news = set(json.load(f))
-else:
-    sent_news = set()
-
-# 紀錄上次檢查日期的文件
-last_checked_date_file = 'last_checked_date.txt'
-if os.path.exists(last_checked_date_file):
-    with open(last_checked_date_file, 'r', encoding='utf-8') as f:
-        last_checked_date = f.read().strip()
-else:
-    last_checked_date = datetime.now().strftime('%Y%m%d')
-
-def save_sent_news():
-    with open(sent_news_file, 'w', encoding='utf-8') as f:
-        json.dump(list(sent_news), f, ensure_ascii=False, indent=4)
-
-def save_last_checked_date(date):
-    with open(last_checked_date_file, 'w', encoding='utf-8') as f:
-        f.write(date)
 
 def get_google_news():
-    url = f'https://news.google.com/search?q=%E6%98%A5%E7%87%95%20when%3A1d&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
+    url = f'https://news.google.com/search?q=%E6%98%A5%E7%87%95%20when%3A4h&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     }
@@ -54,29 +29,17 @@ def get_google_news():
         date_tag = item.find("time", class_="hvbAAd")
         date = date_tag.text if date_tag else "No date" # 日期
 
-        news_id = title
-        if news_id not in sent_news:
-            sent_news.add(news_id)
-            news_list.append({
-                "title": title,
-                "link": link,
-                "source": source,
-                "date": date
-            })
+
+        news_list.append({
+            "title": title,
+            "link": link,
+            "source": source,
+            "date": date
+        })
     
-    save_sent_news()
     return news_list
 
 def check_news():
-    global last_checked_date
-    today = datetime.now().strftime('%Y%m%d')
-    
-    # 如果跨日，清空 sent_news
-    if today != last_checked_date:
-        sent_news.clear()
-        last_checked_date = today
-        save_sent_news()
-        save_last_checked_date(today)
 
     new_news = get_google_news()
 
